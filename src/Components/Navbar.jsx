64e58c117e0219e2d03cfe68c6ec61442a7152c1
cringeuserhere <brendenalves2008@gmail.com>
2026-01-16 08:52:33 -0500

    re

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar(){
         <nav className={styles.navbar}>
             <p className='display'>Our Blooms </p>
             <ul className={styles.ul}>
-                <li className="caption1"><Link to='/gallery'>Gallery</Link></li>
+                <li className="caption1"><Link to='/gallery'>GALLARY</Link></li>
                 <li className="caption1" ><Link to='/about'>ABOUT</Link></li>
                 <li className="caption1"><Link to="/mailto:brendenalves2008@gmail.com">CONTACT</Link></li>
             </ul>
